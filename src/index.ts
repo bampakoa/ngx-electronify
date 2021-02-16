52aa5e0d7f83d74e0539854ee550f4a419921c6f
Jefiozie <jefiozie.bosch@gmail.com>
2021-02-16 15:46:44 +0100

    feat: add builder options

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,31 @@ import {
 import { JsonObject } from '@angular-devkit/core';
 import { spawn } from 'child_process';
 import * as path from 'path';
+interface Options {
+  configuration?: string;
+}
+type BuilderOptions = Options & JsonObject;
 
-export default createBuilder((_: JsonObject, context: BuilderContext) => {
-  return new Promise<BuilderOutput>(async () => {
-    // create a target for building the app
-    const buildTarget = {
-      target: 'serve',
-      project: (context.target as Target).project
-    };
+export default createBuilder(
+  (options: BuilderOptions, context: BuilderContext) => {
+    return new Promise<BuilderOutput>(async () => {
+      // create a target for building the app
+      const buildTarget: Target = {
+        target: 'serve',
+        project: (context.target as Target).project,
+        configuration: options.configuration ? options.configuration : ''
+      };
 
-    // start building the app
-    const build = await context.scheduleTarget(buildTarget);
-    await build.result;
+      // start building the app
+      const build = await context.scheduleTarget(buildTarget);
+      await build.result;
 
-    const electronPath = path.resolve('node_modules/.bin/electron');
-    const appPath = path.resolve(
-      'node_modules/ngx-electronify/dist/renderer.js'
-    );
-    spawn(electronPath, [appPath], { shell: true });
-  });
-});
+      const electronPath = path.resolve('node_modules/.bin/electron');
+      const appPath = path.resolve(
+        'node_modules/ngx-electronify/dist/renderer.js'
+      );
+
+      spawn(electronPath, [appPath], { shell: true });
+    });
+  }
+);

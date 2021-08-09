eb41496fe66b0bbd4d27159157a9e447c1f9d49a
Aristeidis Bampakos <bampakoa@gmail.com>
2021-08-09 23:15:36 +0300

    feat: show Electron window when app is loaded
    Display the native window when the Angular app has finished loading.

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,11 +3,14 @@ import { app, BrowserWindow, shell } from 'electron';
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    show: false
   });
 
   const params = process.argv.slice(2);
   mainWindow.loadURL('http://localhost:' + params[0]);
+
+  mainWindow.once('ready-to-show', () => mainWindow.show());
 }
 
 app.whenReady().then(() => {

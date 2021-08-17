63a67276d2186175535aab14b63589f5beae6137
Aristeidis Bampakos <bampakoa@gmail.com>
2021-08-18 00:45:21 +0300

    ci: update prettier to 2.3.2

--- schematics/index.ts
+++ schematics/index.ts
@@ -11,31 +11,29 @@ interface NgAddOptions extends Schema {
   project: string;
 }
 
-export const ngAdd = (options: NgAddOptions) => async (
-  tree: Tree,
-  _context: SchematicContext
-) => {
-  const { host, workspace } = await getWorkspace(tree);
-  const project = workspace.projects.get(options.project);
+export const ngAdd =
+  (options: NgAddOptions) => async (tree: Tree, _context: SchematicContext) => {
+    const { host, workspace } = await getWorkspace(tree);
+    const project = workspace.projects.get(options.project);
 
-  if (!project) {
-    throw new SchematicsException(
-      'The specified Angular project is not defined in this workspace.'
-    );
-  }
+    if (!project) {
+      throw new SchematicsException(
+        'The specified Angular project is not defined in this workspace.'
+      );
+    }
 
-  if (project.extensions.projectType !== 'application') {
-    throw new SchematicsException(
-      `ngx-electronify requires an Angular project type of "application" in angular.json.`
-    );
-  }
+    if (project.extensions.projectType !== 'application') {
+      throw new SchematicsException(
+        `ngx-electronify requires an Angular project type of "application" in angular.json.`
+      );
+    }
 
-  project.targets.add({
-    name: 'desktop',
-    builder: 'ngx-electronify:electron',
-    options: {}
-  });
+    project.targets.add({
+      name: 'desktop',
+      builder: 'ngx-electronify:electron',
+      options: {}
+    });
 
-  await workspaces.writeWorkspace(workspace, host);
-  return tree;
-};
+    await workspaces.writeWorkspace(workspace, host);
+    return tree;
+  };
